070d244a6ea337dd5a97dcfad4a9c521a5004831
Dalya Al-Amri <50600227+iDalya@users.noreply.github.com>
2022-04-02 07:10:41 +0300

    fixing error

--- APP/__init__.py
+++ APP/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app.views import admin_views
-from app.views import user_views
+from APP.views import admin_views
+from APP.views import user_views
 from app.views import public_views
